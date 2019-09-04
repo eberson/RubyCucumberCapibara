0447279c5ff777498d1be6ae6a0c39694c0e896d
Godoy <patricia.godoy@dxc.com>
2019-09-04 16:01:29 -0300

    commit

---
+++ vetores.rb
@@ -0,0 +1,18 @@
+=begin
+vetor
+
+vetor[1,2,3,4,5]
+
+
+hash
+
+hash = {"keys","valor"}
+
+hashes = {"a" => "1", "b" => "2"}
+hashes["a"];
+hashes = {a: => "1", b: => "2"}
+hashes[:a]
+=end
+
+vetor = [1,2,3,4,5]
+
