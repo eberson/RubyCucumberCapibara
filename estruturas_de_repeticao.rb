1a3013a4730ac7a46da4f6025a456998a7c367e7
Godoy <patricia.godoy@dxc.com>
2019-09-09 12:01:00 -0300

    commit

--- estruturas_de_repeticao.rb
+++ estruturas_de_repeticao.rb
@@ -1,12 +1,12 @@
 i = 1
 
-while i <= 10
+while i <= 10 #executa um bloco enquanto a condição for verdadeira
     puts "Sou verdadeiro #{i}"
     i+=1
 end
 
 j = 10
-until j<=1
+until j<=1 #executa um bloco até que a condição verdadeira seja atendida
     puts "Sou falso #{j}"
     j-=1;
 end
--- vetores.rb
+++ vetores.rb
@@ -1,18 +1,33 @@
 =begin
 vetor
-
+      0,1,2,3,4 - indice inteiro  
 vetor[1,2,3,4,5]
 
-
-hash
-
+hash - arrrays associativos, hash permite que se use qualquer objeto como índice
 hash = {"keys","valor"}
 
+valor
 hashes = {"a" => "1", "b" => "2"}
 hashes["a"];
+
+símbolos
 hashes = {a: => "1", b: => "2"}
 hashes[:a]
+
+vetor = [1,2,3,4,5,6,7,8,9,10]
+
+vetor.each do |i|
+    puts i
+end
+
+
+(1...5).each do |i| #penúltimo elemento
+    puts i
+end
+
+(1..5).each do |i| #último elemento
+    puts i
+end
 =end
 
-vetor = [1,2,3,4,5]
-
+5.times{puts 'patricia'}
