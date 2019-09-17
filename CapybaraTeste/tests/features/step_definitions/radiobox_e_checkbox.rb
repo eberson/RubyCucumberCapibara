d6780132d0b2ee71d8444b9a105e153592fb5970
Godoy <patricia.godoy@dxc.com>
2019-09-17 10:50:06 -0300

    commit

--- CapybaraTeste/tests/features/step_definitions/radiobox_e_checkbox.rb
+++ CapybaraTeste/tests/features/step_definitions/radiobox_e_checkbox.rb
@@ -1,6 +1,15 @@
 Quando("eu marco um checkbox e um radiobox") do
     visit('buscaelementos/radioecheckbox')
 
+    #CheckBox
     find('label[for="white"]').click
+    
+    #id_purple e qu e a label seja visível, elemento não visível
+    check('purple', allow_label_click: true) #marcar o checkbox
+    uncheck('purple', allow_label_click: true) #desmarcar o checkbox
+    
+
+    #Radiobutton
+    choose('red',allow_label_click: true)
     sleep(4)
 end
