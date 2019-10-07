6dbca919dfdd231587326adb156d256e685c07c1
Godoy <patricia.godoy@dxc.com>
2019-10-07 09:47:02 -0300

    commit

--- tests/features/support/helper.rb
+++
@@ -1,9 +0,0 @@
-module Helper
-
-    def tirar_foto(nome_arquivo, resultado)
-        caminho_arquivo = "results/screenshot/test_#{resultado}"
-        foto = "#{caminho_arquivo}/#{nome_arquivo}.png"
-        page.save_screenshot(foto)
-        embed(foto, 'image/png','Clique aqui')
-    end
-end
